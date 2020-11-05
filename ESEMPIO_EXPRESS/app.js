edb202094f56368b393ef3b896d8c6ebc98885c0
RufailG <girgis.rufail@iisgalvanimi.edu.it>
2020-11-05 09:39:39 +0000

    fine 01.1

--- ESEMPIO_EXPRESS/app.js
+++ ESEMPIO_EXPRESS/app.js
@@ -1,8 +1,22 @@
+const path = require('path');
+
 var express = require('express');
-var app = express(); 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var app = express();
+
+
+app.get('/', function(req,res) {
+ res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/about', function(req,res) {
+ res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/sitemap', function(req,res) {
+ res.sendFile(path.join(__dirname, 'sitemap.html'));
+});
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
